78d130f81c90ef1dcb5ce6e29f3dd5a0f4dcae2f
Marcel Lejk <marcel.lejk@educem.net>
2024-10-21 18:37:35 +0200

    Añadir Idiomas
    Idiomas Polaco, Español,Catala e Ingles

--- about.js
+++ about.js
@@ -2,7 +2,9 @@ const playBtn = document.getElementById('play-btn');
 const audioPlayer = document.getElementById('audio-player');
 const trackTitle = document.getElementById('track-title');
 const trackListItems = document.querySelectorAll('.track-list li');
-let currentIndex = 0;
+
+
+// Language content object
 const languageContent = {
     es: {
         titleTrailer: "Trailer del Juego",
@@ -42,52 +44,6 @@ const languageContent = {
     },
 };
 
-function moveRight() {
-    const slider = document.querySelector('.slider');
-    const totalSlides = document.querySelectorAll('.slide').length;
-    currentIndex = (currentIndex + 1) % totalSlides;
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-function moveLeft() {
-    const slider = document.querySelector('.slider');
-    const totalSlides = document.querySelectorAll('.slide').length;
-    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-let isPlaying = false;
-
-playBtn.addEventListener('click', function() {
-    if (isPlaying) {
-        audioPlayer.pause();
-        playBtn.textContent = '▶'; 
-    } else {
-        audioPlayer.play();
-        playBtn.textContent = '❚❚'; 
-    }
-    isPlaying = !isPlaying;
-});
-
-trackListItems.forEach(item => {
-    item.addEventListener('click', function() {
-        const newSrc = this.getAttribute('data-src');
-        const newTitle = this.querySelector('.track-title').textContent;
-
-        audioPlayer.src = newSrc;
-        trackTitle.textContent = `Sea of Stars OST - Eric W. Brown - ${newTitle}`;
-        audioPlayer.play();
-        playBtn.textContent = '❚❚';
-        isPlaying = true;
-    });
-});
-
-document.getElementById('menu-btn').addEventListener('click', function() {
-    var dropdown = document.getElementById('dropdown');
-    dropdown.classList.toggle('hidden'); 
-});
-
-
 // Function to change the language
 function changeLanguage(lang) {
     // Get all elements with the attribute data-i18n
@@ -98,5 +54,3 @@ function changeLanguage(lang) {
         element.textContent = languageContent[lang][key] || element.textContent; // Use languageContent here
     });
 }
-
-
